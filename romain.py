0022a17c2ad69f07bb2979cf9a4d25b595d77586
adarshsingh912786-a11y <adarshsingh912786@gmail.com>
2026-01-24 23:12:04 +0530

    Update the code with the vice verse speciality .py

--- romain.py
+++ romain.py
@@ -1,4 +1,4 @@
-Roman_map = {
+roman_map = {
     'I' : 1,
     'V' : 5,
     'X' : 10,
@@ -8,7 +8,7 @@ Roman_map = {
     'M' : 1000
 }
 
-value = [(1000,'M'),(900,'CM'),(500,'D'),(400,'CD'),(100,'C'),(90,'XC'),(50,'L'),(40,'XL'),(10,'X'),(9,'IX'),(5,'V'),(4,'IV'),(1,'I')]
+value_map = [(1000,'M'),(900,'CM'),(500,'D'),(400,'CD'),(100,'C'),(90,'XC'),(50,'L'),(40,'XL'),(10,'X'),(9,'IX'),(5,'V'),(4,'IV'),(1,'I')]
 
 def roman_to_dec(roman):
 
@@ -16,23 +16,50 @@ def roman_to_dec(roman):
     total = 0
 
     for i in range(len(roman)):
-        if roman[i] not in Roman_map:
+        if roman[i] not in roman_map:
             return None
         
-        value = Roman_map[roman[i]]
+        value = roman_map[roman[i]]
 
-        if i+1 < len(roman) and value < Roman_map.get(roman[i+1]):
+        if i+1 < len(roman) and value < roman_map.get(roman[i+1]):
             total -= value
         else:
             total += value
     
     return total
 
+def dec_to_roman(number):
+    if number <= 0 or number > 3000:
+        return None
+    
+    result = ""
+
+    for value , symbol in value_map:
+        while number >= value:
+            result += symbol
+            number -= value
+    
+    return result
+            
 def main():
-    print("-------Roman Converter-----")
-    user = input("Enter the roman number: ")
-    answer = roman_to_dec(user)
-    print(answer)
+    print("\n-------Roman Converter-----")
+    print("1. Roman to Decimal")
+    print("2. Decimal to Roman")
+
+    choice = input("Choose what to you want to convert (1/2): ")
+
+    if choice == "1":
+        user = input("Enter the roman number: ")
+        answer = roman_to_dec(user)
+        print(f"Decimal Value : {answer}")
+    
+    elif choice == "2":
+        user = int(input("Enter Decimal number (1-3000): "))
+        answer = dec_to_roman(user)
+        print(f"Roman numeral: {answer}")
+    
+    else:
+        print("❌Invalid Choice")
+
 
 main()
-
