5d0679b728ff796d5e58652d78b7f738a9318334
adarshsingh912786-a11y <adarshsingh912786@gmail.com>
2026-02-02 20:58:29 +0530

    Update expensemain delete and view function imported

--- expensemain.py
+++ expensemain.py
@@ -1,6 +1,9 @@
 from dataofexpense import (
     create_table,
-    add_expense
+    add_expense,
+    view_expense,
+    delete_expense
+
 )
 
 def main():
@@ -21,6 +24,28 @@ def main():
             note = input("Enter the reason of expense: ")
             add_expense(amount, category, date, note)
             print("Record Saved successfully")
+
+        elif choice == 2:
+
+            expenses = view_expense()
+            print("\n $$$$$$ All Expenses $$$$$$\n")
+
+            for ex in expenses:
+                print(f"{ex[0]}.{ex[1]}|{ex[2]}|{ex[3]}|{ex[4]}")
+
+        elif choice == 3:
+
+            expenses = view_expense()
+            print("\n $$$$$$ All Expense $$$$$$\n")
+
+            for ex in expenses:
+                print(f"{ex[0]}.{ex[1]}|{ex[2]}|{ex[3]}|{ex[4]}")
+
+            expense_id = int(input("\n Enter the expense id to delete: "))
+
+            delete_expense(expense_id)
+            print("\n One Expense Delete Successfully")        
+
 
         elif choice ==  4:
             print("Done with updation!")
