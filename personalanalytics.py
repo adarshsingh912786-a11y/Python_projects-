9fac1fde3d66b4255076f52085021c99edaa3a2d
adarshsingh912786-a11y <adarshsingh912786@gmail.com>
2026-02-09 12:09:36 +0530

    Update personalanalytics added compare month

--- personalanalytics.py
+++ personalanalytics.py
@@ -50,3 +50,32 @@ def get_top_category(month):
     if top:
         return top[0], top[1]
     return None
+
+def compare_month(month):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    split_string = month.split(sep="-")
+    year = int(split_string[0])
+    mon = int(split_string[1])
+
+    if mon > 1:
+        mon = mon - 1
+    elif mon == 1:
+        year = year - 1
+        mon = 12
+    new_year = str(year)
+    new_month = str(mon)
+
+    pattern = f"{new_year}-0{new_month}%"
+
+    cursor.execute("""
+        SELECT SUM(amount) FROM expenses WHERE date LIKE ?
+""",(pattern, ))
+    
+    row = cursor.fetchone()
+    prev_total = row[0] if row[0] is not None else 0
+
+    conn.close()
+    return  prev_total
